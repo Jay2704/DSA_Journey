888e22531396e7dc8ef57226dcea9ccfca7ebf80
Jay2704 <decoder.2704@gmail.com>
2024-06-18 15:29:25 -0400

    Update v7.py

--- recursion/v7.py
+++ recursion/v7.py
@@ -24,20 +24,56 @@ def allSubseq(ind, ds, s, total,arr,n):
 
             # Complete it
 
-def oneSubseq(ind, ds, s, total,arr,n):
+# def oneSubseq(ind, ds, s, total,arr,n):
 
+#     if ind == n:
+#         if s == total:
+#             print(ds)
+#         return True
+    
+#     ds.append(arr[ind])
+#     s += arr[ind]
+#     return oneSubseq(ind+1,ds,s,total,arr,n)
+
+#     s -= arr[ind]
+#     ds.pop()
+#     return oneSubseq(ind+1,ds,s,total,arr,n)
+
+
+
+
+
+def findOneSubseq(ind, ds, s, total, arr, n):
     if ind == n:
         if s == total:
-            print(ds)
-            return True
-    
+            return ds.copy()  # Return a copy of the current subsequence
+        return None  # No valid subsequence found at this branch
+
+    # Include the current element
     ds.append(arr[ind])
     s += arr[ind]
-    return oneSubseq(ind+1,ds,s,total,arr,n)
+    result = findOneSubseq(ind + 1, ds, s, total, arr, n)
+    if result is not None:
+        return result  # Valid subsequence found, return it
 
+    # Exclude the current element
     s -= arr[ind]
     ds.pop()
-    return oneSubseq(ind+1,ds,s,total,arr,n)
+    result = findOneSubseq(ind + 1, ds, s, total, arr, n)
+    if result is not None:
+        return result  # Valid subsequence found, return it
+
+    return None  # No valid subsequence found in any branch
+
+def printOneSubseq(arr, total):
+    n = len(arr)
+    result = findOneSubseq(0, [], 0, total, arr, n)
+    if result:
+        print(result)
+    else:
+        print("No valid subsequence found")
+
+
 
 
 
@@ -45,4 +81,9 @@ n = int(input("Enter number of Elements : "))
 total = int(input("Enter the Sum : "))
 arr = list(map(int,input("Enter Elements : ").split()))
 ds = []
+print("Printing all Subsequence")
 allSubseq(0, ds, 0, total, arr, n)
+print("Printing One Subsequence")
+printOneSubseq(arr, total)
+
+
