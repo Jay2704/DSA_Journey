2477afe371328905312b31b32a7fa533d75764be
Jay-2704 <mp11252@umbc.edu>
2025-03-22 16:56:19 -0400

    sadfwseF

--- Binary_Trees/bst.py
+++ Binary_Trees/bst.py
@@ -88,7 +88,7 @@ class BST:
             return None
         if root.val > n1 and root.val > n2:
             return self.find_lca(root.left, n1, n2)
-        if root.val < n1 and root.val < n2:
+        elif root.val < n1 and root.val < n2:
             return self.find_lca(root.right, n1, n2)
         return root
 
