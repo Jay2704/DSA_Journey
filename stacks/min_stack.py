10b3bd24683b30ddb23ac7b09d259f7b323f913f
Jay2704 <decoder.2704@gmail.com>
2025-07-29 17:46:26 -0400

    Update Min Stack implementation and add subtitle converters

--- stacks/min_stack.py
+++ stacks/min_stack.py
@@ -11,23 +11,4 @@ class MinStack:
             min_val = val
         self.stack.append(val, min_val)
 
-    def pop(self):
-        if self.stack:
-            return self.stack.pop()[0]
-        return None
     
-    def top(self):
-        if self.stack:
-            return self.stack[-1][0]
-        return None
-    
-    def get_min(self):      
-        if self.stack:
-            return self.stack[-1][1]
-        return None
-    
-    def is_empty(self):
-        return len(self.stack) == 0
-    
-    def size(self):
-        return len(self.stack)
