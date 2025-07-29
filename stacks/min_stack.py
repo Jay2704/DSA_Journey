df69ffb1098cbc606f7f8fb7a6725515da54b6fb
Jay2704 <decoder.2704@gmail.com>
2025-07-29 18:56:58 -0400

    Added min stack by me

--- stacks/min_stack.py
+++ stacks/min_stack.py
@@ -1,14 +1,36 @@
 import math
 
-class MinStack:
+
+class MinStack(self):
     def __init__(self):
         self.stack = []
 
     def push(self, val):
-        if self.stack:
-            min_val = min(val, self.stack[-1][1])
+        if not self.stack:
+            self.stack.append((val, val))
         else:
-            min_val = val
-        self.stack.append(val, min_val)
+            self.stack.append((val, min(val, self.stack[-1][1])))
 
+    def pop(self):
+        if not self.stack.is_empty():
+            return self.stack.pop()[0]
+        else:
+            raise IndexError("Pop from empty stack")
     
+    def top(self):
+        if not self.is_empty():
+            return self.stack[-1][0]
+        else:
+            raise IndexError("Top from an empty stack")
+
+    def get_min(self):
+        if not self.is_empty():
+            return self.stack[-1][1]
+        else:
+            raise IndexError("Get_min from an empty stack")
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
