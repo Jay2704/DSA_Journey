931876cd80b9efbb70aa9b68fe08ad45bf13d503
Jay2704 <decoder.2704@gmail.com>
2025-09-02 10:19:52 -0400

    trapping Rain water

--- stacks/trapping_rain_water.py
+++ stacks/trapping_rain_water.py
@@ -1,12 +1,4 @@
 
 
-def trap(height):
-    left = 0
-    right = len(height) - 1
-
-    left_max = height[left]
-    right_max = height[right]
-
-    
-    
-print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
+def solution1(height):
+    pass
